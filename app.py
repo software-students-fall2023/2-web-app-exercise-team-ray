a2bb53d4d8f0a8a76b3781cbe5fd7d79dcb7f820
YimengDuan2002 <123713674+YimengDuan2002@users.noreply.github.com>
2023-10-20 16:47:50 -0400

    Add files via upload

---
+++ app.py
@@ -0,0 +1,147 @@
+from flask import Flask, render_template, request, redirect, url_for, make_response
+from dotenv import load_dotenv
+import os
+import pymongo
+import datetime
+from bson.objectid import ObjectId
+import sys
+import os
+import subprocess
+
+# instantiate the app
+app = Flask(__name__)
+# turn on debugging if in development mode
+if os.getenv('FLASK_ENV', 'development') == 'development':
+    # turn on debugging, if in development
+    app.debug = True # debug mnode
+# connect to the database
+cxn = pymongo.MongoClient(os.getenv('MONGO_URI'), serverSelectionTimeoutMS=5000)
+try:
+    # verify the connection works by pinging the database
+    cxn.admin.command('ping') # The ping command is cheap and does not require auth.
+    db = cxn[os.getenv('MONGO_DBNAME')] # store a reference to the database
+    print(' *', 'Connected to MongoDB!') # if we get here, the connection worked!
+except Exception as e:
+    # the ping command failed, so the connection is not available.
+    print(' *', "Failed to connect to MongoDB at", os.getenv('MONGO_URI'))
+    print('Database connection error:', e) # debug
+
+
+
+@app.route('/')
+def home():
+    """
+    Route for the home page
+    """
+    return render_template('index.html')
+
+@app.route('/read')
+def read():
+    """
+    Route for GET requests to the read page.
+    Displays some information for the user with links to other pages.
+    """
+    docs = db.exampleapp.find({}).sort("created_at", -1) # sort in descending order of created_at timestamp
+    return render_template('read.html', docs=docs) # render the read template
+
+@app.route('/create')
+def create():
+    """
+    Route for GET requests to the create page.
+    Displays a form users can fill out to create a new document.
+    """
+    return render_template('create.html') # render the create template
+
+
+@app.route('/create', methods=['POST'])
+def create_post():
+    """
+    Route for POST requests to the create page.
+    Accepts the form submission data for a new document and saves the document to the database.
+    """
+    name = request.form['fname']
+    message = request.form['fmessage']
+
+    # create a new document with the data the user entered
+    doc = {
+        "name": name,
+        "message": message, 
+        "created_at": datetime.datetime.utcnow()
+    }
+    db.exampleapp.insert_one(doc) # insert a new document
+    return redirect(url_for('read')) # tell the browser to make a request for the /read route
+
+    @app.route('/edit/<mongoid>')
+def edit(mongoid):
+    """
+    Route for GET requests to the edit page.
+    Displays a form users can fill out to edit an existing record.
+    """
+    doc = db.exampleapp.find_one({"_id": ObjectId(mongoid)})
+    return render_template('edit.html', mongoid=mongoid, doc=doc) # render the edit template
+
+
+@app.route('/edit/<mongoid>', methods=['POST'])
+def edit_post(mongoid):
+    """
+    Route for POST requests to the edit page.
+    Accepts the form submission data for the specified document and updates the document in the database.
+    """
+    name = request.form['fname']
+    message = request.form['fmessage']
+
+    doc = {
+        # "_id": ObjectId(mongoid), 
+        "name": name, 
+        "message": message, 
+        "created_at": datetime.datetime.utcnow()
+    }
+
+    db.exampleapp.update_one(
+        {"_id": ObjectId(mongoid)}, # match criteria
+        { 
+            "$set": doc }
+    )
+ return redirect(url_for('read')) # tell the browser to make a request for the /read route
+
+
+@app.route('/delete/<mongoid>')
+def delete(mongoid):
+    """
+    Route for GET requests to the delete page.
+    Deletes the specified record from the database, and then redirects the browser to the read page.
+    """
+    db.exampleapp.delete_one({"_id": ObjectId(mongoid)})
+    return redirect(url_for('read')) # tell the web browser to make a request for the /read route.
+
+@app.route('/webhook', methods=['POST'])
+def webhook():
+    """
+    GitHub can be configured such that each time a push is made to a repository, GitHub will make a request to a particular web URL... this is called a webhook.
+    This function is set up such that if the /webhook route is requested, Python will execute a git pull command from the command line to update this app's codebase.
+    You will need to configure your own repository to have a webhook that requests this route in GitHub's settings.
+    Note that this webhook does do any verification that the request is coming from GitHub... this should be added in a production environment.
+    """
+    # run a git pull command
+    process = subprocess.Popen(["git", "pull"], stdout=subprocess.PIPE)
+    pull_output = process.communicate()[0]
+    # pull_output = str(pull_output).strip() # remove whitespace
+    process = subprocess.Popen(["chmod", "a+x", "flask.cgi"], stdout=subprocess.PIPE)
+    chmod_output = process.communicate()[0]
+    # send a success response
+    response = make_response('output: {}'.format(pull_output), 200)
+    response.mimetype = "text/plain"
+    return response
+
+@app.errorhandler(Exception)
+def handle_error(e):
+    """
+    Output any errors - good for debugging.
+    """
+    return render_template('error.html', error=e) # render the edit template
+
+
+if __name__ == "__main__":
+    #import logging
+    #logging.basicConfig(filename='/home/ak8257/error.log',level=logging.DEBUG)
+    app.run(debug = True)
